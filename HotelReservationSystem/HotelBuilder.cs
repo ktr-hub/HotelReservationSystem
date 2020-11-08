5c4597194ce2eef5693dda47b43e4e090799cdfd
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-11-08 11:52:35 +0530

    [Tirupathi] Add. Retrieve rated hotel with good rating

--- HotelReservationSystem/HotelBuilder.cs
+++ HotelReservationSystem/HotelBuilder.cs
@@ -75,5 +75,48 @@ namespace HotelReservationSystem
             }
 
         }
+
+        /// <summary>
+        /// UC7-RatedHotel
+        /// </summary>
+        /// <param name="checkInDate"></param>
+        /// <param name="checkOutDate"></param>
+        public void RatedHotel(DateTime checkInDate, DateTime checkOutDate)
+        {
+            //Finding hotel rate for each hotel between specified dates and finally printing max of them
+
+            Dictionary<Hotel, double> mapHotelToTotalRate = new Dictionary<Hotel, double>();
+
+            foreach (Hotel hotel in hotelDictionary.Values)
+            {
+                double hotelRate = 0;
+                DateTime dateIterator = checkInDate;
+
+                while (checkOutDate >= dateIterator)
+                {
+                    string day = dateIterator.DayOfWeek.ToString();
+
+                    hotelRate += (day.Equals("Saturday")) ? hotel.WeekEndRate : hotel.WeekDayRate;
+
+                    dateIterator = dateIterator.AddDays(1);
+                }
+
+                mapHotelToTotalRate.Add(hotel, hotelRate);
+            }
+
+            var maxValue = mapHotelToTotalRate.Values.Max();
+
+            var records = from hotel in mapHotelToTotalRate
+                          where hotel.Value == maxValue
+                          orderby hotel.Key.Rating descending
+                          select hotel;
+
+            Console.WriteLine("\n\nAvailable Rated Hotels : ");
+            foreach (var record in records.Take(1))
+            {
+                Console.WriteLine("\n\n-->Hotel \t\t: " + record.Key.HotelName + "\n  -->Rating \t\t: " + record.Key.Rating + "\n  -->Total Rate \t: " + mapHotelToTotalRate[record.Key]);
+            }
+
+        }
     }
 }
--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -12,8 +12,8 @@ namespace HotelReservationSystem
             hotelBuilder.addHotel("Lakewood", 130, 90,"Regular",3);
             hotelBuilder.addHotel("Bridgewood", 160, 60,"Regular",4);
             hotelBuilder.addHotel("Ridgewood", 220, 150,"Regular",5);
-            hotelBuilder.cheapestHotel(DateTime.Parse("09/11/2020"),DateTime.Parse("09/12/2020"));
-
+            //hotelBuilder.cheapestHotel(DateTime.Parse("09/11/2020"),DateTime.Parse("09/12/2020"));
+            hotelBuilder.RatedHotel(DateTime.Parse("09/11/2020"), DateTime.Parse("09/12/2020"));
         }
     }
 }
